6071ebdb920725371c6beac266b9929bfb82ea1e
Saurav Dhillon <souravdhillon010@gmail.com>
2024-11-23 18:21:15 -0500

    Updates were made to index.js file

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const { fetchMyIP } = require('./iss');
+const { fetchCoordsByIP } = require('./iss');
 
 fetchMyIP((error, ip) => {
   if (error) {
@@ -7,4 +8,11 @@ fetchMyIP((error, ip) => {
   }
 
   console.log("It worked! Returned IP:", ip);
+  fetchCoordsByIP(ip, (error, coord) => {
+    if (error) {
+      console.log("It didn't work fetching coordinates", error);
+      return;
+    }
+    console.log("Here are your geopolitical coordinates", coord);
+  });
 });
