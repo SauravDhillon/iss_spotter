84904124983e98ef32919c911f51ebf37072dcd0
Saurav Dhillon <souravdhillon010@gmail.com>
2024-11-23 19:51:22 -0500

    Updates were made to index.js file

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const { fetchMyIP } = require('./iss');
 const { fetchCoordsByIP } = require('./iss');
 const { fetchISSFlyOverTimes } = require('./iss');
+const { nextISSTimesForMyLocation } = require('./iss');
 
 fetchMyIP((error, ip) => {
   if (error) {
@@ -24,4 +25,18 @@ fetchMyIP((error, ip) => {
 
     });
   });
+});
+const printPassTimes = function(passTimes) {
+  for (const pass of passTimes){
+    const datetime = new Date(0);
+    datetime.setUTCSeconds(pass.risetime);
+    const duration = pass.duration;
+    console.log(`Next pass at ${datetime} for ${duration} seconds!`);
+  }
+}
+nextISSTimesForMyLocation((error, passTimes) => {
+  if(error){
+    return console.log("It didn't work!", error);
+  }
+  printPassTimes(passTimes);
 });
